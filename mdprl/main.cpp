225c091843e540d9a29ccfae0d4d50ae6a202fa4
Henry Tominaga <htominaga@grubhub.com>
2018-09-03 14:32:55 -0700

    Initial Commit

---
+++ mdprl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mdprl
+//
+//  Created by Henry Tominaga on 9/3/18.
+//  Copyright © 2018 Henry Tominaga. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
